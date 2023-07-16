41462d31b0b6d2631eadee921ef234fc27eab549
falya128 <falya128@users.noreply.github.com>
2023-07-16 10:08:29 +0900

    CDK プロジェクトの新規作成

---
+++ test/skyway-vue-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SkywayVueCdk from '../lib/skyway-vue-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/skyway-vue-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SkywayVueCdk.SkywayVueCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
